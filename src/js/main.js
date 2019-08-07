e905735597375b24d575280e0711ef0046a222d3
monirul-islam777 <mislam@iquantile.com>
2019-08-07 16:19:08 +0600

    page is now responsive

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ window.onload = function () {
     var img = new Image();
     img.src = 'assets/banner-dynamic.png';
 
+//We can call an ajax here to load the image from the backend and set that to img.src thus the banner image can change dynamically in future
+
+
     img.onload = function () {
         // CREATE CANVAS CONTEXT.
         var canvas = document.getElementById('canvas');
